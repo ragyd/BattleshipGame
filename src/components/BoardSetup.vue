4cb8dbb1215fd55dc080f1cce4ab697d395ca4d8
Sandy Garcia <sfgcusi@gmail.com>
2018-08-16 17:34:56 -0400

    Update BoardSetup.vue

--- src/components/BoardSetup.vue
+++ src/components/BoardSetup.vue
@@ -1,88 +1,68 @@
 <template>
-	<div class="board-setup">
-		<div class="board-container">
-			<table id="board">
-				<tr v-for="(row, rowIndex) in rows" v-bind:key="row">
-					<td v-for="(col, colIndex) in cols" :id="(rowIndex + 1)+'-'+(colIndex + 1)" v-bind:key="col">
-						0
-					</td>
-				</tr>
-			</table>
-		</div>
-		<div id="ship-container">
-			<div title="ship1"><img src="../../imgs/alabama.png" class="ship-img"></div>
-			<div title="ship2"><img src="../../imgs/yamato.png" class="ship-img"></div>
-			<div title="ship3"><img src="../../imgs/conqueror.png" class="ship-img"></div>
-			<div title="ship4"><img src="../../imgs/musashi.png" class="ship-img"></div>
-			<div title="ship5"><img src="../../imgs/destroyer.png" class="ship-img"></div>
-		</div>
-		<button>Rotate</button>
-		<button>Save</button>
-	</div>
+  <div class="board-setup">
+    <div class="board-container">
+      <table id="board">
+        <tr v-for="(row, rowIndex) in rows" v-bind:key="row">
+          <td v-for="(col, colIndex) in cols" :id="(rowIndex + 1)+'-'+(colIndex + 1)" v-bind:key="col">
+            0
+          </td>
+        </tr>
+      </table>
+    </div>
+    <div id="ship-container">
+      <div title="ship1"><img src="../../imgs/alabama.png" class="ship-img"></div>
+      <div title="ship2"><img src="../../imgs/yamato.png" class="ship-img"></div>
+      <div title="ship3"><img src="../../imgs/conqueror.png" class="ship-img"></div>
+      <div title="ship4"><img src="../../imgs/musashi.png" class="ship-img"></div>
+      <div title="ship5"><img src="../../imgs/destroyer.png" class="ship-img"></div>
+    </div>
+    <button>Rotate</button>
+    <button>Save</button>
+  </div>
 </template>
 <script>
 const dragula = require('dragula');
 
 export default {
-	name: 'BoardSetup',
-	data() {
-		return {
-			cols: 0,
-			rows: 0
-		}
-	},
-	mounted() {
-		this.getBoard()
-			.then(board => {
-				this.cols = board.cols
-				this.rows = board.rows
-			})
-			.then(() => {
-				for(let i = 1; i <= this.cols; i++) {
-					for(let j = 1; j <= this.cols; j++) {
-						dragula([
-							document.getElementById('ship-container'),
-							document.getElementById(i+'-'+j)], {
-							copy: true
-						})
-					}
-				}
-			})
-	},
-	methods: {
-		getBoard() {
-			return Promise.resolve({
-				rows: 10,
-				cols: 15
-			})
-		}
-	}
+  name: 'BoardSetup',
+  data() {
+    return {
+      cols: 0,
+      rows: 0
+    }
+  },
+  mounted() {
+    this.getBoard()
+      .then(board => {
+        this.cols = board.cols
+        this.rows = board.rows
+      })
+      .then(() => {
+        for(let i = 1; i <= this.cols; i++) {
+          for(let j = 1; j <= this.cols; j++) {
+            dragula([
+              document.getElementById('ship-container'),
+              document.getElementById(i+'-'+j)], {
+              copy: true
+            })
+          }
+        }
+      })
+  },
+  methods: {
+    getBoard() {
+      return Promise.resolve({
+        rows: 10,
+        cols: 15
+      })
+    }
+  }
 }
 </script>
 
 <style>
-#drag-elements {
-  display: block;
-  background-color: #dfdfdf;
-  border-radius: 5px;
-  min-height: 50px;
-  margin: 0 auto;
-  padding: 2em;
-}
-
-#drop-target {
-  border: 2px dashed #D9D9D9;
-  border-radius: 5px;
-  min-height: 200px;
-  margin: 0 auto;
-  margin-top: 10px;
-  padding: 2em;
-  display: block;
-  text-align: center;
-}
-
 .ship-img {
-	height: 20px;
-	width: 30px;
+  height: 20px;
+  width: 30px;
 }
 </style>
